b3c6177039f91c5c8550bef04bc6d1ff238985e7
douglasitsoares <dogametal@hotmail.com>
2022-01-03 15:12:31 -0300

    002_Angular_Post_Output_Emmiter

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,18 +1,21 @@
-import { Component } from "@angular/core";
-
+import { Component, EventEmitter, Output } from "@angular/core";
 
 @Component({
-  selector: 'app-post-create',
-  templateUrl: './post-create.component.html'
-
+  selector: "app-post-create",
+  templateUrl: "./post-create.component.html",
+  styleUrls: ["./post-create.component.css"]
 })
-export class PostCreateComponent{
-  // This is very interesting replacemente HtmlTextArea for example set to #postInput for example in HTML
-  enteredValue ='';
-  newPost = 'Insert something';
+export class PostCreateComponent {
+  enteredTitle = "";
+  enteredContent = "";
+  @Output() postCreated = new EventEmitter();
 
-  onAddPost(){
-    this.newPost = this.enteredValue;
+  onAddPost() {
+    const post = {
+      title: this.enteredTitle,
+      content: this.enteredContent
+    };
+    this.postCreated.emit(post);
+    console.log(post);
   }
-
 }
