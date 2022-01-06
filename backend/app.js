ab427b021aa230edda8a85443fe4e0e3204cfd90
douglasitsoares <dogametal@hotmail.com>
2022-01-06 09:54:53 -0300

    012_CORS_Treatment_Nodes_Success

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+
+app.use((req, res, next) =>{
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader("Access-Control-Allow-Header",
+   "Origin, X-Request-With, Content-Type, Accept"
+   );
+   res.setHeader("Access-Control-Allow-Mehtods",
+     "GET, POST, PATCH, OPTIONS");
+   next();
+});
+
 app.use ('/api/posts',(req, res, next) => {
 
   const posts = [
