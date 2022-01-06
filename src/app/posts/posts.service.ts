2dc712ebafbd1f53ac73f74c592e7b5e66d2fcff
douglasitsoares <dogametal@hotmail.com>
2022-01-06 12:22:13 -0300

    011_PreparingBackendForMongo

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -31,8 +31,12 @@ getPostUpdateListener(){
 addPost(title:string, content:string){
 
   const post : Post = {id:null, title:title, content:content}
-  this.posts.push(post); //This is to push a new post into the posts retrieved
-  this.postsUpdated.next ([...this.posts]);
+  this.http.post<{message:string}>('http://localhost:3000/api/posts',post)
+   .subscribe((responseData) => {
+     console.log(responseData);
+     this.posts.push(post); //This is to push a new post into the posts retrieved
+     this.postsUpdated.next ([...this.posts]);
+   });
 }
 
 }
