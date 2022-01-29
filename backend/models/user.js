13e05cf0f85e7106ce45f1046185d029af3382fd
douglasitsoares <dogametal@hotmail.com>
2022-01-29 11:32:13 -0300

    039_informationTokenWorks

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 
 
 const userSchema = mongoose.Schema({
-  email: { type: String, required: true, unique: true },
+  //email: { type: String, required: true, unique: true },
+  email: { type: String, required: true },
   password: { type: String, required: true },
 });
 
