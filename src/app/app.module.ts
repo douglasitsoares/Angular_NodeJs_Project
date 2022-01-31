f7e455cb74a92369cdd46dcc580c3d3695e1a538
douglasitsoares <dogametal@hotmail.com>
2022-01-31 07:02:04 -0300

    041_Implements_InitialInterceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule, FormsModule } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 import {
   MatInputModule,
   MatCardModule,
@@ -20,6 +20,7 @@ import { LoginComponent } from "./auth/login/Login.component";
 import { SignupComponent } from "./auth/signup/Signup.component";
 import { PostListComponent } from "./posts/post-list/post-list.component";
 import { AppRoutingModule } from "./app-routing.module";
+import { AuthInterceptor } from "./auth/auth-interceptor";
 
 @NgModule({
   declarations: [
@@ -45,7 +46,7 @@ import { AppRoutingModule } from "./app-routing.module";
     MatPaginatorModule,
     HttpClientModule
   ],
-  providers: [],
+  providers: [{provide : HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi:true}],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
