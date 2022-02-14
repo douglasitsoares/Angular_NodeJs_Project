d035b8029380d126e8d30a727891088fc04a2f23
douglasitsoares <dogametal@hotmail.com>
2022-02-14 15:48:22 -0300

    002_Middleware_Muter

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,46 +1,15 @@
 const express = require("express");
-const multer = require("multer");
 
 const checkAuth = require ("../middleware/check-auth");
-
+const extractFile = require ("../middleware/importfile");
 const postController = require("../controllers/posts");
 
 const router = express.Router();
 
-const MIME_TYPE_MAP = {
-  "image/png": "png",
-  "image/jpeg": "jpg",
-  "image/jpg": "jpg"
-};
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    const isValid = MIME_TYPE_MAP[file.mimetype];
-    let error = new Error("Invalid mime type");
-    if (isValid) {
-      error = null;
-    }
-    cb(error, "backend/images");
-  },
-  filename: (req, file, cb) => {
-    const name = file.originalname
-      .toLowerCase()
-      .split(" ")
-      .join("-");
-    const ext = MIME_TYPE_MAP[file.mimetype];
-    cb(null, name + "-" + Date.now() + "." + ext); // here renamed picture in case needed
-  }
-});
-
 // Applying to try to callback image on the request body
-router.post(
-  "",
-  checkAuth, // This is to see if you have access to post or not
-  multer({ storage: storage }).single("image"), postController.postCreate);
+router.post( "", checkAuth, extractFile, postController.postCreate);
 
-router.put("/:id",
-  checkAuth,
-  multer({ storage: storage }).single('image'), postController.postUpdate);
+router.put("/:id",checkAuth, extractFile, postController.postUpdate);
 
 router.get("", postController.fetchingPosts);
 
